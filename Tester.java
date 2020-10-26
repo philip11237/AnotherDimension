4a99e7714934027f7c760ba3b192327f4fc9ff2a
philip von mueffling <philip.vm3@gmail.com>
2020-10-25 21:44:10 -0400

    added empty array to sum function to make sure it works after reading second set of instructions from master konstantinovich

--- Tester.java
+++ Tester.java
@@ -2,7 +2,9 @@ public class Tester{
     public static void main (String []args){
       int[] sumtest1={1,2,3,4};
       int[] sumtest2={1,2,3,4,5};
+      int[] sumtest3={};
       System.out.println (ArrayOps.sum(sumtest1));
       System.out.println (ArrayOps.sum(sumtest2));
+      System.out.println (ArrayOps.sum(sumtest3));
     }
 }
